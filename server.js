ddff0b370aeaa8780b71c46c5bb2545baca95375
HUG0L0ZAN0 <A01383795@tec.mx>
2025-05-21 10:11:28 -0600

    CORS

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const authRouter = require('./routes/auth');
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const cors = require('cors');
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use('/api/auth', authRouter);
 
